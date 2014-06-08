d1c91102f17ef1eaed75bb3f6208a7d7f6164922
Yomguithereal <guillaumeplique@gmail.com>
2014-06-08 21:34:49 +0200

    Adding headers options

--- daj/daj.py
+++ daj/daj.py
@@ -37,12 +37,23 @@ class Reader(object):
     def readYaml(self, filename):
         return yaml.load(self.read(filename))
 
-    def readCsv(self, filename, delimiter=','):
-        with codecs.open(filename, 'r') as f:
-            return [i for i in csv.reader(f, delimiter=delimiter)]
+    def readCsv(self, filename, encoding='utf-8', delimiter=',', headers=False):
+        with codecs.open(filename, 'r', encoding=encoding) as f:
+            if headers:
+                reader = csv.reader(f, delimiter=delimiter)
+                h = reader.next()
+                a = []
+                for r in reader:
+                    o = {}
+                    for j, e in enumerate(r):
+                        o[h[j]] = e
+                    a.append(o)
+                return a
+            else:
+                return [i for i in csv.reader(f, delimiter=delimiter)]
 
-    def readTsv(self, filename):
-        return self.readCsv(filename, delimiter='\t')
+    def readTsv(self, filename, headers=False):
+        return self.readCsv(filename, delimiter='\t', headers=headers)
 
 
 class Writer(object):
@@ -54,35 +65,74 @@ class Writer(object):
 #===========
 class Daj(object):
 
-    supported = [
-        '.json',
-        '.yml',
-        '.yaml',
-        '.csv',
-        '.tsv'
-    ]
-
+    # Properties
     reader = Reader()
+
+    # Constructor
+    def __init__(self, kind=None):
+        self.kind = kind
+
+    # Writing
+    #--------
 
     # Call operator as writer
     def __call__(self, data):
         return Writer()
 
+    # Reading
+    #--------
+
+    # Reader abstract
+    def read(self, ext, filename):
+        ext = self.kind or ext
+
+        if ext == '.json':
+            return self.reader.readJson(filename)
+        elif ext == '.yml' or ext == '.yaml':
+            return self.reader.readYaml(filename)
+        elif ext == '.csv':
+            return self.reader.readCsv(filename)
+        elif ext == '.tsv':
+            return self.reader.readTsv(filename)
+        elif ext == '.csvh':
+            return self.reader.readCsv(filename, headers=True)
+        elif ext == '.tsvh':
+            return self.reader.readTsv(filename, headers=True)
+        else:
+            return self.reader.read(filename)
+
     # Less than operator as read file
     def __lt__(self, filename):
         ext = extension(filename)
+        return self.read(ext, filename)
 
-        if ext in self.supported:
-            if ext == '.json':
-                return self.reader.readJson(filename)
-            elif ext == '.yml' or ext == '.yaml':
-                return self.reader.readYaml(filename)
-            elif ext == '.csv':
-                return self.reader.readCsv(filename)
-            else:
-                return self.reader.readTsv(filename)
-        else:
-            return self.reader.read(filename)
+    @property
+    def json(self):
+        return Daj('.json')
+
+    @property
+    def yaml(self):
+        return Daj('.yml')
+
+    @property
+    def yml(self):
+        return Daj('.yml')
+
+    @property
+    def csv(self):
+        return Daj('.csv')
+
+    @property
+    def csvh(self):
+        return Daj('.csvh')
+
+    @property
+    def tsv(self):
+        return Daj('.tsv')
+
+    @property
+    def tsvh(self):
+        return Daj('.tsvh')
 
 
 # Exporting an instance of daj
--- tests/read_test.py
+++ tests/read_test.py
@@ -25,10 +25,20 @@ class ReaderTest(unittest.TestCase):
             daj < self.res('hello.json')
         )
 
+        self.assertEqual(
+            {u'hello': u'world', u'colors': [u'yellow', u'blue']},
+            daj.json < self.res('hello.json')
+        )
+
     def test_yaml(self):
         self.assertEqual(
             {u'hello': u'world', u'colors': [u'yellow', u'blue']},
             daj < self.res('hello.yml')
+        )
+
+        self.assertEqual(
+            {u'hello': u'world', u'colors': [u'yellow', u'blue']},
+            daj.yaml < self.res('hello.yml')
         )
 
     def test_csv(self):
@@ -37,8 +47,28 @@ class ReaderTest(unittest.TestCase):
             daj < self.res('names.csv')
         )
 
+        self.assertEqual(
+            [['Joe', 'Dagger'], ['Elena', 'Ashcroft']],
+            daj.csv < self.res('names.csv')
+        )
+
+        self.assertEqual(
+            [{'Firstname': 'Joe', 'Lastname': 'Dagger'}, {'Firstname': 'Elena', 'Lastname': 'Ashcroft'}],
+            daj.csvh < self.res('hnames.csv')
+        )
+
     def test_tsv(self):
         self.assertEqual(
             [['Joe', 'Dagger'], ['Elena', 'Ashcroft']],
             daj < self.res('names.tsv')
+        )
+
+        self.assertEqual(
+            [['Joe', 'Dagger'], ['Elena', 'Ashcroft']],
+            daj.tsv < self.res('names.tsv')
+        )
+
+        self.assertEqual(
+            [{'Firstname': 'Joe', 'Lastname': 'Dagger'}, {'Firstname': 'Elena', 'Lastname': 'Ashcroft'}],
+            daj.tsvh < self.res('hnames.tsv')
         )
